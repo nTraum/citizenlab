9bb0cfe0d23ed64eeb672802bd60262a0bb530cf
Sara <sara@citizenlab.co>
2021-09-07 16:26:45 +0200

    fixes path

--- front/app/services/actionCable/consumer.ts
+++ front/app/services/actionCable/consumer.ts
@@ -1,11 +1,15 @@
-import { API_PATH } from 'containers/App/constants';
+import { API_HOST, API_PORT } from 'containers/App/constants';
 import { createConsumer } from '@rails/actioncable';
 import { getJwt } from 'utils/auth/jwt';
+
+import * as ActionCable from '@rails/actioncable';
+
+ActionCable.logger.enabled = true;
 
 const getWebSocketURL = () => {
   const token = getJwt();
   console.log(token);
-  return `${API_PATH}/cable?token=${token}`;
+  return `ws:${API_HOST}:${API_PORT}/cable?token=${token}`;
 };
 
 // Use a function to dynamically generate the URL
