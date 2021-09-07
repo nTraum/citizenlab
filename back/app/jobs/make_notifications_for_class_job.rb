90a2bb872781d68cda76989683655cade74835b0
Sébastien Hoorens <sebastien@citizenlab.co>
2021-09-07 15:19:56 +0200

    serialized notification

--- back/app/jobs/make_notifications_for_class_job.rb
+++ back/app/jobs/make_notifications_for_class_job.rb
@@ -11,7 +11,22 @@ class MakeNotificationsForClassJob < ApplicationJob
         notification.save!
         LogActivityJob.set(wait: 10.seconds).perform_later(notification, 'created', notification.recipient, notification.created_at.to_i)
         if notification.type == 'Notifications::CommentOnYourComment'
-          ActionCable.server.broadcast('comment_on_your_comment', { body: 'Someone commented on your comment' })
+          serialized_notification = {
+            id: notification.id,
+            attributes: {
+              post_type: notification.post_type,
+              initiating_user_first_name: notification.initiating_user&.first_name,
+              initiating_user_last_name: notification.initiating_user&.last_name,
+              initiating_user_slug: notification.initiating_user&.slug,
+              post_title_multiloc: notification.post&.title_multiloc,
+              post_slug: notification.post&.slug
+            },
+            read_at: notification.read_at,
+            created_at: notification.created_at,
+            type: notification.type.demodulize.underscore,
+            recipient: recipient_id
+          }
+          ActionCable.server.broadcast('comment_on_your_comment', serialized_notification)
         end
       rescue ActiveRecord::RecordInvalid => exception
         Raven.capture_exception(exception)
