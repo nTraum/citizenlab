54d1221a708d171fb94fb0a058aa867ff0c19e4c
Simon Tharby <simontharby@gmail.com>
2022-02-04 13:51:19 +0000

    only import React once

--- front/app/containers/IdeasShow/Buttons/IdeaFollowButtonComponent.tsx
+++ front/app/containers/IdeasShow/Buttons/IdeaFollowButtonComponent.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import IdeaCTAButton from './IdeaCTAButton';
 import IdeaCTAButtonToggled from './IdeaCTAButtonToggled';
 
