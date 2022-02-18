9e434a300c36fabbad60671240b4e4a6a1c1c90f
Philipp Preß <ntraum@fastmail.com>
2022-02-18 14:52:12 +0100

    Fix wrong number of arguments for translate method call

--- back/engines/free/email_campaigns/app/mailers/email_campaigns/application_mailer.rb
+++ back/engines/free/email_campaigns/app/mailers/email_campaigns/application_mailer.rb
@@ -36,7 +36,7 @@ module EmailCampaigns
 
     def format_message(key, component: nil, escape_html: true, values: {})
       group = component || @campaign.class.name.demodulize.underscore
-      msg = t("email_campaigns.#{group}.#{key}", values)
+      msg = t("email_campaigns.#{group}.#{key}", **values)
       escape_html ? msg : msg.html_safe
     end
 
