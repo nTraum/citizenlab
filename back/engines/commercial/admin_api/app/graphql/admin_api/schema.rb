c1fe3fd109871144cd2af83681f52f8e771aecc0
Philipp Preß <ntraum@fastmail.com>
2022-02-18 14:30:16 +0100

    Remove maybe unused MutationType

--- back/engines/commercial/admin_api/app/graphql/admin_api/schema.rb
+++ back/engines/commercial/admin_api/app/graphql/admin_api/schema.rb
@@ -2,8 +2,6 @@ module AdminApi
   class Schema < GraphQL::Schema
     # Required:
     query Types::QueryType
-    # Optional:
-    mutation Types::MutationType
 
     default_max_page_size 100
   end
